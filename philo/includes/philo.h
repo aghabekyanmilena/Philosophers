9f54800aea725f8cf5ea03cb72b134cd1717bfff
Milena Aghabekyan <miaghabe@c2r1s2.42yerevan.am>
2025-05-20 20:35:33 +0400

    new commit

--- philo/includes/philo.h
+++ philo/includes/philo.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   philo.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: alisharu <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: miaghabe <miaghabe@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/06 15:42:34 by miaghabe          #+#    #+#             */
-/*   Updated: 2025/05/20 17:53:26 by alisharu         ###   ########.fr       */
+/*   Updated: 2025/05/20 19:51:22 by miaghabe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,8 +29,9 @@
 # define CALLOC_ERROR 5
 
 typedef struct s_philo	t_philo;
+typedef struct s_table	t_table;
 
-typedef struct s_table
+struct s_table
 {
 	long			philo_count;
 	long			time_to_die;
@@ -44,9 +45,9 @@ typedef struct s_table
 	pthread_mutex_t	start_time_mutex;
 	pthread_mutex_t	*forks;
 	t_philo			*philo;
-}	t_table;
+};
 
-typedef struct s_philo
+struct s_philo
 {
 	int				index;
 	int				eat_count;
@@ -56,28 +57,27 @@ typedef struct s_philo
 	pthread_mutex_t	*right_fork;
 	pthread_t		thread;
 	t_table			*table;
-}	t_philo;
+};
 
-// utils
+// validation
 int		validation(int argc, char **argv);
 int		validate_arguments(int argc, char **argv);
-void	*ft_calloc(size_t count, size_t size);
-long	ft_atol(const char *str);
-void	error_handling(int num);
+
+// init
 int		init_philo(t_table *table);
 t_table	*init_table(int argc, char **argv);
-void	free_table(t_table *table);
-void	free_philo(t_table *table);
 int		init_mutexes(t_table *table);
 
+// free
+void	free_table(t_table *table);
+void	free_philo(t_table *table);
 
-////
-
-void	print_error(void);
+// utils
+void	print_action(t_philo *philo, const char *msg);
+void	error_handling(int num);
 long	get_time_in_ms(void);
-
-// parsing
-void	parse_arguments(t_table *table, char **argv);
+void	*ft_calloc(size_t count, size_t size);
+long	ft_atol(const char *str);
 
 
 
--- philo/src/action.c
+++ philo/src/action.c
@@ -3,29 +3,37 @@
 /*                                                        :::      ::::::::   */
 /*   action.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: alisharu <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: miaghabe <miaghabe@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/05/20 18:10:00 by alisharu          #+#    #+#             */
-/*   Updated: 2025/05/20 18:46:23 by alisharu         ###   ########.fr       */
+/*   Created: 2025/05/20 19:17:57 by miaghabe          #+#    #+#             */
+/*   Updated: 2025/05/20 19:51:10 by miaghabe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-void	print_action(t_philo *philo)
+void	print_action(t_philo *philo, const char *msg)
 {
-	philo->table-
+	long	timestamp;
+
+	pthread_mutex_lock(&philo->table->print_mutex);
+	if (!philo->table->program_stop)
+	{
+		timestamp = get_time_in_ms() - philo->table->start_time;
+		printf("%ld %d %s\n", timestamp, philo->index, msg);
+	}
+	pthread_mutex_unlock(&philo->table->print_mutex);
 }
 
 void	pick_forks(t_philo *philo)
 {
 	if (philo->index % 2 == 0)
 	{
-		pthread_mutex_lock(philo->left_fork)
-		printf
+		pthread_mutex_lock(philo->left_fork);
+		// printf
 	}
 }
-void	philo_eating(t_philo *philo)
-{
+// void	philo_eating(t_philo *philo)
+// {
 	
-}
+// }
